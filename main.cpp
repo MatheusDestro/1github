219064bd752da7b658ef6e71e4ca0d0a5a100976
matheus.destro <matheus.destro@LAD0211>
2017-09-14 18:51:30 -0300

    Comentario 2

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main() {
 
 	std::cout << "Bom Dia!!" << std::endl;  // Irá imprimir Bom Dia!!
-	std::cout << "Boa Tarde!!" << std::endl;
+	std::cout << "Boa Tarde!!" << std::endl; // Irá imprimir Boa Tarde!!
 	system("pause");
 
 	return 0;
